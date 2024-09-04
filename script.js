44584eeab4c94572c398cb4e61a027030b805783
mubeen0308 <abdulmubeen2006@gmail.com>
2024-09-04 22:09:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//template literals 
+
+let x;
+
+const name = 'mubeen';
+const age= '17';
+
+x = `hello,my name is ${name} and I am ${age} years old `;
+
+//string properties and methods 
+
+const s = new String ('Hello world');
+
+x=typeof s;
+
+x = s.length;
+
+//ACCESS value by number key
+x = s[1];
+
+x = s.__proto__;
+
+console.log(x);
